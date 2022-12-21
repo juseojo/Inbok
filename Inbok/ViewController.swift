77c1251f85004a1f883a93b08c52f8098716aa3d
juseojo <j86371224@gmail.com>
2022-12-21 15:03:07 +0900

    Initial Commit

---
+++ Inbok/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Inbok
+//
+//  Created by seongjun cho on 2022/12/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
