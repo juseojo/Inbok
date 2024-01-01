15dc483e490b9089a8b2c0189e704da0962fd55a
juseojo <j86371224@gmail.com>
2024-01-01 21:02:09 +0900

    sending and index remove

--- Inbok/ViewController/chat_ViewController.swift
+++ Inbok/ViewController/chat_ViewController.swift
@@ -10,32 +10,55 @@ import UIKit
 import RealmSwift
 
 class Chat_ViewController: UIViewController {
-    
-    let chat_view = Chat_view()
-    let chat_viewModel = Chat_viewModel()
 
+	var index: Int
+    let chat_view = Chat_view()
+	lazy var chat_viewModel = Chat_viewModel(index)
+	
+	init(index: Int) {
+		self.index = index
+		super.init(nibName: nil, bundle: nil)
+	}
+
+	required init?(coder: NSCoder) {
+		fatalError("init(coder:) has not been implemented")
+	}
+	
 	@objc func back_btn_click(_ sender: UIButton)
 	{
 		self.tabBarController?.tabBar.isHidden = false
 		self.navigationController?.popViewController(animated:true)
 	}
+	
+	@objc func send_btn_click(_ sender: UIButton)
+	{
+		chat_viewModel.send(message: chat_view.chat_text_view.text)
+		chat_view.chat_text_view.text = ""
+	}
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
+		//basic set
 		self.tabBarController?.tabBar.isHidden = true
 		view.backgroundColor = UIColor(named: "BACKGROUND")
 		self.navigationController?.isNavigationBarHidden = true
 
+		//back gesture
 		let back_gesture = UIScreenEdgePanGestureRecognizer(target: self, action: #selector(self.back_btn_click(_:)))
 		back_gesture.edges = .left
 		view.addGestureRecognizer(back_gesture)
 
+		
+		//button set
 		chat_view.back_btn.addTarget(self, action: #selector(back_btn_click(_:)), for: .touchUpInside
 		)
 		
-		chat_view.chat_text_view.delegate = self
+		chat_view.chat_send_button.addTarget(self, action: #selector(send_btn_click(_:)),
+			for: .touchUpInside
+		)
 		
+		chat_view.chat_text_view.delegate = self
 		
 		
         self.view.addSubview(chat_view)
@@ -62,7 +85,13 @@ extension Chat_ViewController: UITextViewDelegate {
 	func textViewDidChange(_ textView: UITextView) {
 		let line_num = textView.numberOfLine()
 
+		//text view's height change by line's number
 		switch line_num {
+		case 1:
+			textView.superview?.snp.updateConstraints{ (make) in
+				make.height.equalTo(50)
+			}
+			break
 		case 2:
 			textView.superview?.snp.updateConstraints{ (make) in
 				make.height.equalTo(70)
