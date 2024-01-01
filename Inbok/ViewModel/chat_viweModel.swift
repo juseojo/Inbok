3df31032ced9fff477af88f330faa6647708aa7f
juseojo <j86371224@gmail.com>
2024-01-01 21:03:42 +0900

    index remove and chat_DB(List<Chat>) make

--- Inbok/ViewModel/chat_viweModel.swift
+++ Inbok/ViewModel/chat_viweModel.swift
@@ -12,45 +12,43 @@ import RealmSwift
 class Chat_viewModel {
 	
 	let chat_model: Chat_model
+	var index: Int
 	
-	init(){
+	init(_ index: Int){
+		self.index = index
 		self.chat_model = Chat_model()
 	}
 	
-	func send(listener : String, message : String)
+	func send(message : String)
 	{
+		let text = "\(UserDefaults.standard.string(forKey: "name") ?? ""):\(message)"
+		let realm = try! Realm()
+		let chat_obj : Chat = realm.objects(Chat_DB.self).first?.chat_list[index] ?? Chat()
+		let listener = chat_obj.talker.name
 		let conn = RMQConnection(uri: "amqp://admin:123690@43.202.245.98:5672/%2F",
 								 delegate: RMQConnectionDelegateLogger())
+
+		print(listener)
+
 		conn.start()
 		
 		let ch = conn.createChannel()
 		let q = ch.queue(listener)
-		ch.defaultExchange().publish(message.data(using: .utf8)!, routingKey: q.name)
-		print("sent")
-				
-		let _ = add_realm(
-			realm: chat_model.chat_DB,
-			name: UserDefaults.standard.string(forKey: "name")!,
-			text: message,
-			time: Date().toString(),
-			profile_image: UserDefaults.standard.string(forKey: "profile_image")!)
+		ch.defaultExchange().publish(text.data(using: .utf8)!, routingKey: q.name)
+		print("sent : \(text)")
 		
-		conn.close()
-	}
-	
-	func add_realm(realm: Realm,name: String, text: String, time: String, profile_image: String) -> Message {
 		
-		let message = Message()
-		
-		message.name = name
-		message.text = text
-		message.time = time
-		message.profile_image = profile_image
-		
+		let message_obj = Message()
+
+		message_obj.name = UserDefaults.standard.string(forKey: "name") ?? ""
+		message_obj.text = message
+		message_obj.time = Date().toString()
+		message_obj.profile_image = UserDefaults.standard.string(forKey: "profile_image") ?? ""
+
 		try! realm.write {
-			realm.add(message)
+			chat_obj.chatting.append(message_obj)
 		}
 		
-		return message
+		//conn.close()
 	}
 }
